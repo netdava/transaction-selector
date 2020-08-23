93a6be264652fb0e56a8c0e0dc96753110113529
Andrei Stan <andrei.stan@greatpeopleinside.com>
2020-08-23 19:28:33 +0300

    Added transaction dispatcher.

--- src/ofx-converter.js
+++ src/ofx-converter.js
@@ -16,15 +16,104 @@ function transactionAmt(credit, debit) {
 }
 
 function mapTransaction(tx) {
-    const { credit, debit, date, txNumber, title, details, parsedDetails } = tx;
-    return {
-        TRNTYPE: transactionType(credit, debit),
-        DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
-        TRNAMT: transactionAmt(credit, debit),
-        NAME: parsedDetails.Terminal,
-        FITID: `x-${date}-${txNumber}`,
-        MEMO: details.toString(),
-        REFNUM: parsedDetails.Referinta,
+    const { credit, debit, title, date, txNumber, details, parsedDetails } = tx;
+
+    switch (title) {
+        case 'Cumparare POS':
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: parsedDetails.Terminal,
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+                REFNUM: parsedDetails.Referinta,
+            }
+            break;
+        case 'Taxe si comisioane':
+            return {
+                TRNTYPE: 'DEBIT',
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: 'ING BANK',
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+                REFNUM: parsedDetails.Referinta,
+            }
+            break;
+        case 'Comision pe operatiune ':
+            return {
+                TRNTYPE: 'DEBIT',
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: 'ING BANK',
+                FITID: `x-${date}-${txNumber}`,
+            }
+            break;
+        case 'Plata debit direct':
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: parsedDetails.Beneficiar,
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+                REFNUM: parsedDetails.Referinta,
+            }
+            break;
+        case 'Actualizare dobanda':
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: "ING BANK",
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: title,
+            }
+            break;
+        case 'Incasare':
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+                REFNUM: parsedDetails.Referinta,
+            }
+            break;
+        case 'Retragere numerar':
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: parsedDetails.Terminal,
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+            }
+            break;
+        case "Transfer Home'Bank":
+            return {
+                TRNTYPE: transactionType(credit, debit),
+                DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                TRNAMT: transactionAmt(credit, debit),
+                NAME: parsedDetails.Beneficiar,
+                FITID: `x-${date}-${txNumber}`,
+                MEMO: details.toString(),
+                REFNUM: parsedDetails.Referinta,
+            }
+            break;
+            case 'Cumparare POS corectie':
+                return {
+                    TRNTYPE: transactionType(credit, debit),
+                    DTPOSTED: dateFns.format(new Date(date), "yyyyMMdd"),
+                    TRNAMT: transactionAmt(credit, debit),
+                    NAME: parsedDetails.Terminal,
+                    FITID: `x-${date}-${txNumber}`,
+                    MEMO: details.toString(),
+                }
+                break;
+        default:
+            throw (new Error(`Operatiune necunoscuta - ${title}`));
     }
 };
 
